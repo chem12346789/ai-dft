05a26086ba8fcec9d0d4523d221b4d5c07daa8ef
auto <chenzihao@bright91.cm.cluster>
2024-01-17 13:36:55 +0800

    240117

--- gen_pot_ene.py
+++ gen_pot_ene.py
@@ -141,28 +141,33 @@ parser.add_argument(
 
 args = parser.parse_args()
 
+logger = logging.getLogger(__name__)
+logging.StreamHandler.terminator = ""
 if len(args.distance_list) == 3:
     distance_l = np.linspace(
         args.distance_list[0], args.distance_list[1], int(args.distance_list[2])
     )
-    path_dir = (
-        path
-        / f"data-{args.molecular}-{args.basis}-{args.method}-{args.level}_{distance_l[0]}_{distance_l[1]}_{distance_l[2]}"
+    path_dir = path / f"data-{args.molecular}-{args.basis}-{args.method}-{args.level}"
+    if not path_dir.exists():
+        path_dir.mkdir(parents=True)
+    Path(path_dir / f"inv_{distance_l[0]}_{distance_l[1]}_{distance_l[2]}.log").unlink(
+        missing_ok=True
+    )
+    logger.addHandler(
+        logging.FileHandler(
+            path_dir / f"inv_{distance_l[0]}_{distance_l[1]}_{distance_l[2]}.log"
+        )
     )
 else:
     distance_l = args.distance
+    path_dir = path / f"data-{args.molecular}-{args.basis}-{args.method}-{args.level}"
+    if not path_dir.exists():
+        path_dir.mkdir(parents=True)
+    Path(path_dir / f"inv.log").unlink(missing_ok=True)
+    logger.addHandler(logging.FileHandler(path_dir / f"inv.log"))
+logger.setLevel(logging.DEBUG)
 
 molecular = Mol[args.molecular]
-
-if not path_dir.exists():
-    path_dir.mkdir(parents=True)
-
-logger = logging.getLogger(__name__)
-logging.StreamHandler.terminator = ""
-# clear the log
-Path(path_dir / "inv.log").unlink(missing_ok=True)
-logger.addHandler(logging.FileHandler(path_dir / "inv.log"))
-logger.setLevel(logging.DEBUG)
 
 for distance in distance_l:
     molecular[0][1] = distance
