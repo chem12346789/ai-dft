be31c201e459c6b71c9344aff16b507ed04750f1
auto <chenzihao@bright91.cm.cluster>
2024-05-02 17:06:23 +0800

    240502

--- train_script/train-template.bash
+++ train_script/train-template.bash
@@ -14,4 +14,4 @@ export OPENBLAS_NUM_THREADS=12
 export PYTHONPATH=~/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
-~/anaconda3/bin/python train.py -dl -0.5 0.5 21 -b cc-pvdz --extend_atom 0 1 --extend_xyz 0 1 2 --eval_step 25 --load CHECKPOINT
+~/anaconda3/bin/python train.py -dl -0.25 0.25 11 -b cc-pvdz --extend_atom 0 1 --extend_xyz 0 1 2 --eval_step 25 --load CHECKPOINT --epoch 5000000
