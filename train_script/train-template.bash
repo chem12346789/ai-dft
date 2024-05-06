972e69d53e5f4437770e690a1f3009648d62f197
czh <pb142060@mail.ustc.edu.cn>
2024-05-06 17:48:12 +0800

    240506

--- train_script/train-template.bash
+++ train_script/train-template.bash
@@ -15,4 +15,4 @@ export OPENBLAS_NUM_THREADS=12
 export PYTHONPATH=~/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
-~/anaconda3/bin/python train.py -dl -0.5 0.5 41 -b cc-pvdz --extend_atom 0 1 --extend_xyz 0 1 2 --eval_step 50 --adam ADAM --batch_size 100 --epoch 5000000
+~/anaconda3/bin/python train.py -dl -0.5 0.5 41 -b cc-pvdz --extend_atom 0 1 --extend_xyz 0 1 2 --eval_step 50 --adam ADAM --batch_size 128 --epoch 50000
