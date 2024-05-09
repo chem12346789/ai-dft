3b08fbd3c5b6c2790a525ff1a1ea4fdd0447ab84
auto <chenzihao@bright91.cm.cluster>
2024-05-09 16:38:06 +0800

    240508

--- validate.py
+++ validate.py
@@ -5,7 +5,6 @@ ATOM_LIST = [
     "C",
 ]
 TRAIN_STR_DICT = [
-<<<<<<< HEAD
     "Methane",
     "Ethane",
     "Ethylene",
@@ -25,27 +24,6 @@ TRAIN_STR_DICT = [
     "Cyclobutane",
     "Spiropentane",
     "Benzene",
-=======
-    # "Methane",
-    # "Ethane",
-    # "Ethylene",
-    # "Acetylene",
-    # "Allene",
-    # "Cyclopropene",
-    # "Propyne",
-    # "Cyclopropane",
-    # "Propylene",
-    # "Propane",
-    # "Isobutane",
-    # "Butane",
-    # "Butadiene",
-    # "Butyne",
-    # "Bicyclobutane",
-    # "Cyclopropylmethyl",
-    # "Cyclobutane",
-    # "Spiropentane",
-    # "Benzene",
->>>>>>> 896dc6a96888e38f2cd9f12520515a3f665b55e6
 ]
 EVAL_STR_DICT = [
     "Pentane",
