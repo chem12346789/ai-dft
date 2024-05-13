044dddd032ca0bb11790a64fe036ccd78c012d3c
czh <pb142060@mail.ustc.edu.cn>
2024-05-13 15:09:31 +0800

    240513

--- cadft/utils/DataBase.py
+++ cadft/utils/DataBase.py
@@ -142,122 +142,142 @@ class DataBase:
             if if_equilibrium:
                 if abs(distance) > 1e-3:
                     continue
-            name = f"{name_mol}_{extend_atom}_{extend_xyz}_{distance:.4f}"
-            print(f"\rCheck {name:>30}", end="")
-            name_train.append(name)
 
-            molecular = copy.deepcopy(Mol[name_mol])
-            molecular[extend_atom][extend_xyz] += distance
-            dft2cc = cadft.cc_dft_data.CC_DFT_DATA(
-                molecular,
-                name=name,
-                basis=self.args.basis,
-                if_basis_str=self.args.if_basis_str,
+            ene_loss_i, rho_loss_i, name = self.check_iter(
+                name_mol,
+                extend_atom,
+                extend_xyz,
+                distance,
+                model_list,
             )
 
-            dm1_middle = np.zeros((dft2cc.mol.nao, dft2cc.mol.nao))
-            dm1_middle_real = np.zeros((dft2cc.mol.nao, dft2cc.mol.nao))
-            exc = 0
+            ene_loss.append(ene_loss_i)
+            rho_loss.append(rho_loss_i)
+            name_train.append(name)
 
-            for i, j in product(range(dft2cc.mol.natm), range(dft2cc.mol.natm)):
-                if molecular[i][0] != molecular[j][0]:
-                    key = f"{molecular[i][0]}-{molecular[j][0]}"
+        return ene_loss, rho_loss, name_train
+
+    def check_iter(
+        self,
+        name_mol,
+        extend_atom,
+        extend_xyz,
+        distance,
+        model_list=None,
+    ):
+        """
+        Check the input data, if model_list is not none, check loss of the model.
+        """
+        name = f"{name_mol}_{extend_atom}_{extend_xyz}_{distance:.4f}"
+        print(f"\rCheck {name:>30}", end="")
+
+        molecular = copy.deepcopy(Mol[name_mol])
+        molecular[extend_atom][extend_xyz] += distance
+        dft2cc = cadft.cc_dft_data.CC_DFT_DATA(
+            molecular,
+            name=name,
+            basis=self.args.basis,
+            if_basis_str=self.args.if_basis_str,
+        )
+
+        dm1_middle = np.zeros((dft2cc.mol.nao, dft2cc.mol.nao))
+        dm1_middle_real = np.zeros((dft2cc.mol.nao, dft2cc.mol.nao))
+        exc = 0
+
+        for i, j in product(range(dft2cc.mol.natm), range(dft2cc.mol.natm)):
+            if molecular[i][0] != molecular[j][0]:
+                key = f"{molecular[i][0]}-{molecular[j][0]}"
+            else:
+                if i == j:
+                    key = f"{molecular[i][0]}-{molecular[j][0]}-D"
                 else:
-                    if i == j:
-                        key = f"{molecular[i][0]}-{molecular[j][0]}-D"
-                    else:
-                        key = f"{molecular[i][0]}-{molecular[j][0]}-O"
+                    key = f"{molecular[i][0]}-{molecular[j][0]}-O"
 
-                input_mat = self.input[key][f"{name}_{i}_{j}"]
-                middle_real = self.middle[key][f"{name}_{i}_{j}"]
-                dm1_middle_real[
+            input_mat = self.input[key][f"{name}_{i}_{j}"]
+            middle_real = self.middle[key][f"{name}_{i}_{j}"]
+            dm1_middle_real[
+                dft2cc.atom_info["slice"][i], dft2cc.atom_info["slice"][j]
+            ] = middle_real.reshape(NAO[molecular[i][0]], NAO[molecular[j][0]])
+            output_real = self.output[key][f"{name}_{i}_{j}"]
+
+            if model_list is None:
+                output_mat = output_real.copy()
+                dm1_middle[
                     dft2cc.atom_info["slice"][i], dft2cc.atom_info["slice"][j]
-                ] = middle_real.reshape(NAO[molecular[i][0]], NAO[molecular[j][0]])
-                output_real = self.output[key][f"{name}_{i}_{j}"]
-
-                if model_list is None:
-                    output_mat = output_real.copy()
+                ] = dm1_middle_real[
+                    dft2cc.atom_info["slice"][i], dft2cc.atom_info["slice"][j]
+                ].copy()
+            else:
+                if not self.args.only_2:
+                    input_mat = (
+                        torch.as_tensor(input_mat.copy())
+                        .to(torch.float64)
+                        .contiguous()
+                        .to(device=self.device)
+                    )
+                    middle_mat = model_list[key + "1"](input_mat)
+                    output_mat = model_list[key + "2"](middle_mat)
+                    middle_mat = middle_mat.detach().cpu().numpy()
+                    output_mat = output_mat.detach().cpu().numpy()
+                    dm1_middle[
+                        dft2cc.atom_info["slice"][i], dft2cc.atom_info["slice"][j]
+                    ] = middle_mat.reshape(NAO[molecular[i][0]], NAO[molecular[j][0]])
+                else:
                     dm1_middle[
                         dft2cc.atom_info["slice"][i], dft2cc.atom_info["slice"][j]
                     ] = dm1_middle_real[
                         dft2cc.atom_info["slice"][i], dft2cc.atom_info["slice"][j]
                     ].copy()
-                else:
-                    if not self.args.only_2:
-                        input_mat = (
-                            torch.as_tensor(input_mat.copy())
-                            .to(torch.float64)
-                            .contiguous()
-                            .to(device=self.device)
-                        )
-                        middle_mat = model_list[key + "1"](input_mat)
-                        output_mat = model_list[key + "2"](middle_mat)
-                        middle_mat = middle_mat.detach().cpu().numpy()
-                        output_mat = output_mat.detach().cpu().numpy()
-                        dm1_middle[
-                            dft2cc.atom_info["slice"][i], dft2cc.atom_info["slice"][j]
-                        ] = middle_mat.reshape(
-                            NAO[molecular[i][0]], NAO[molecular[j][0]]
-                        )
-                    else:
-                        dm1_middle[
-                            dft2cc.atom_info["slice"][i], dft2cc.atom_info["slice"][j]
-                        ] = dm1_middle_real[
-                            dft2cc.atom_info["slice"][i], dft2cc.atom_info["slice"][j]
-                        ].copy()
-                        middle_mat = (
-                            torch.as_tensor(middle_real.copy())
-                            .to(torch.float64)
-                            .contiguous()
-                            .to(device=self.device)
-                        )
-                        output_mat = model_list[key + "2"](middle_mat)
-                        output_mat = output_mat.detach().cpu().numpy()
+                    middle_mat = (
+                        torch.as_tensor(middle_real.copy())
+                        .to(torch.float64)
+                        .contiguous()
+                        .to(device=self.device)
+                    )
+                    output_mat = model_list[key + "2"](middle_mat)
+                    output_mat = output_mat.detach().cpu().numpy()
 
-                exc += output_mat[0]
+            exc += output_mat[0]
 
-            mdft = pyscf.scf.RKS(dft2cc.mol)
-            mdft.xc = "b3lyp"
-            mdft.kernel()
-            eri = dft2cc.mol.intor("int2e")
-            h1e = dft2cc.mol.intor("int1e_nuc") + dft2cc.mol.intor("int1e_kin")
-            coords = mdft.grids.coords
-            weights = mdft.grids.weights
-            ao_value = dft.numint.eval_ao(dft2cc.mol, coords, deriv=1)
+        mdft = pyscf.scf.RKS(dft2cc.mol)
+        mdft.xc = "b3lyp"
+        mdft.kernel()
+        eri = dft2cc.mol.intor("int2e")
+        h1e = dft2cc.mol.intor("int1e_nuc") + dft2cc.mol.intor("int1e_kin")
+        coords = mdft.grids.coords
+        weights = mdft.grids.weights
+        ao_value = dft.numint.eval_ao(dft2cc.mol, coords, deriv=1)
 
-            ek_mat_cc = np.einsum("pqrs,pr,qs->qs", eri, dm1_middle, dm1_middle)
-            rho = dft.numint.eval_rho(dft2cc.mol, ao_value, dm1_middle, xctype="GGA")
-            exc_cc_grids = dft.libxc.eval_xc("b3lyp", rho)[0]
-            exc_cc = (
-                np.einsum("i,i,i->", exc_cc_grids, rho[0], weights)
-                - np.sum(ek_mat_cc) * 0.05
-            )
-            e_dft = (
-                exc_cc
-                + np.einsum("pqrs,pq,rs", eri, dm1_middle, dm1_middle) / 2
-                + np.sum(h1e * dm1_middle)
-                + dft2cc.mol.energy_nuc()
-            )
-            if model_list is None:
-                ene_loss_i = exc + 1000 * (e_dft - self.data[name]["e_cc"])
-                if ene_loss_i > 1e-3:
-                    print("")
-                    print(f"name: {name}, ene_loss_i: {ene_loss_i:7.4f}")
-                rho_loss_i = 0
-            else:
-                ene_loss_i = exc + 1000 * (e_dft - self.data[name]["e_cc"])
-
-                rho_real = dft.numint.eval_rho(dft2cc.mol, ao_value[0], dm1_middle_real)
-                rho_loss_i = np.einsum("i,i->", np.abs(rho[0] - rho_real), weights)
-            print(
-                f"    ene_loss: {ene_loss_i:7.4f} rho_loss:  {rho_loss_i:7.4f}",
-                end="",
-            )
-            ene_loss.append(ene_loss_i)
-            rho_loss.append(rho_loss_i)
-
-        return (
-            ene_loss,
-            rho_loss,
-            name_train,
+        ek_mat_cc = np.einsum("pqrs,pr,qs->qs", eri, dm1_middle, dm1_middle)
+        rho = dft.numint.eval_rho(dft2cc.mol, ao_value, dm1_middle, xctype="GGA")
+        exc_cc_grids = dft.libxc.eval_xc("b3lyp", rho)[0]
+        exc_cc = (
+            np.einsum("i,i,i->", exc_cc_grids, rho[0], weights)
+            - np.sum(ek_mat_cc) * 0.05
         )
+        e_dft = (
+            exc_cc
+            + np.einsum("pqrs,pq,rs", eri, dm1_middle, dm1_middle) / 2
+            + np.sum(h1e * dm1_middle)
+            + dft2cc.mol.energy_nuc()
+        )
+
+        if model_list is None:
+            ene_loss_i = exc + 1000 * (e_dft - self.data[name]["e_cc"])
+            if ene_loss_i > 1e-3:
+                print("")
+                print(f"name: {name}, ene_loss_i: {ene_loss_i:7.4f}")
+
+            rho_loss_i = 0
+        else:
+            ene_loss_i = exc + 1000 * (e_dft - self.data[name]["e_cc"])
+
+            rho_real = dft.numint.eval_rho(dft2cc.mol, ao_value[0], dm1_middle_real)
+            rho_loss_i = np.einsum("i,i->", np.abs(rho[0] - rho_real), weights)
+
+        print(
+            f"    ene_loss: {ene_loss_i:7.4f} rho_loss:  {rho_loss_i:7.4f}",
+            end="",
+        )
+
+        return ene_loss_i, rho_loss_i, name
