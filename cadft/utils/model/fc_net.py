29f4984909901d9b4e7d7679990bbdfd49e97db8
auto <chenzihao@bright91.cm.cluster>
2024-05-15 12:01:39 +0800

    240515

--- cadft/utils/model/fc_net.py
+++ cadft/utils/model/fc_net.py
@@ -11,15 +11,26 @@ class FCNet(nn.Module):
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.laynorm1 = nn.LayerNorm(hidden_size)
         self.relu1 = nn.ReLU()
+
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.laynorm2 = nn.LayerNorm(hidden_size)
         self.relu2 = nn.ReLU()
+
         self.fc3 = nn.Linear(hidden_size, hidden_size)
         self.laynorm3 = nn.LayerNorm(hidden_size)
         self.relu3 = nn.ReLU()
+
         self.fc4 = nn.Linear(hidden_size, hidden_size)
+        self.laynorm4 = nn.LayerNorm(hidden_size)
         self.relu4 = nn.ReLU()
-        self.fc5 = nn.Linear(hidden_size, output_size)
+
+        self.fc5 = nn.Linear(hidden_size, hidden_size)
+        self.laynorm5 = nn.LayerNorm(hidden_size)
+        self.relu5 = nn.ReLU()
+
+        self.fcout1 = nn.Linear(hidden_size, hidden_size)
+        self.reluout = nn.ReLU()
+        self.fcout2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         """
@@ -28,13 +39,24 @@ class FCNet(nn.Module):
         out = self.fc1(x)
         out = self.laynorm1(out)
         out = self.relu1(out)
+
         out = self.fc2(out)
         out = self.laynorm2(out)
         out = self.relu2(out)
+
         out = self.fc3(out)
         out = self.laynorm3(out)
         out = self.relu3(out)
+
         out = self.fc4(out)
+        out = self.laynorm4(out)
         out = self.relu4(out)
+
         out = self.fc5(out)
+        out = self.laynorm5(out)
+        out = self.relu5(out)
+
+        out = self.fcout1(out)
+        out = self.reluout(out)
+        out = self.fcout2(out)
         return out
--- gen_data_script/gen_data_template.bash
+++ gen_data_script/gen_data_template.bash
@@ -15,4 +15,4 @@ export PYSCF_TMPDIR=~/workdir/tmp
 export PYTHONPATH=~/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
-~/anaconda3/bin/python gen_dm_ene.py -dl START END STEP -b BASIS --extend_atom 0 1 --extend_xyz 0 1 2 --name_mol MOL
+~/anaconda3/bin/python gen_dm_ene.py -dl START END STEP -b BASIS --extend_atom 2 --extend_xyz 0 1 2 --name_mol MOL
