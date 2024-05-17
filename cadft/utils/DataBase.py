0db774c4520f5bf570ce2a76d464bdab6c569138
czh <pb142060@mail.ustc.edu.cn>
2024-05-17 17:34:04 +0800

    240516

--- cadft/utils/DataBase.py
+++ cadft/utils/DataBase.py
@@ -71,7 +71,7 @@ class DataBase:
                 continue
 
             name = f"{name_mol}_{extend_atom}_{extend_xyz}_{distance:.4f}"
-            if not (self.dir_weight / f"e_ccsd_{name}.npy").exists():
+            if not (self.dir_output / f"output_cc_dft_diff_{name}.npy").exists():
                 print(
                     f"\rNo file: {name_mol:>20}_{extend_atom}_{extend_xyz}_{distance:.4f}",
                     end="",
@@ -87,8 +87,12 @@ class DataBase:
         e_cc = np.load(self.dir_weight / f"e_ccsd_{name}.npy")
         e_dft = np.load(self.dir_weight / f"e_dft_{name}.npy")
         energy_nuc = np.load(self.dir_weight / f"energy_nuc_{name}.npy")
-        delta_exc_cc = np.load(self.dir_output / f"output_delta_exc_cc_{name}.npy")
-        # cc_dft_diff = np.load(self.dir_output / f"output_cc_dft_diff_{name}.npy")
+        aoslice_by_atom = np.load(self.dir_weight / f"aoslice_by_atom_{name}.npy")
+
+        input_mat = np.load(self.dir_input / f"input_dft_{name}.npy")
+        middle_mat = np.load(self.dir_input / f"input_cc_{name}.npy")
+        output_mat = np.load(self.dir_output / f"output_delta_exc_cc_{name}.npy")
+        # output_mat = np.load(self.dir_output / f"output_cc_dft_diff_{name}.npy")
 
         self.data[name] = {
             "e_cc": e_cc,
@@ -100,6 +104,9 @@ class DataBase:
         natom = len(molecular)
 
         for i, j in product(range(natom), range(natom)):
+            slice_i = slice(aoslice_by_atom[i][0], aoslice_by_atom[i][1])
+            slice_j = slice(aoslice_by_atom[j][0], aoslice_by_atom[j][1])
+            slice_ = (slice_i, slice_j)
             if molecular[i][0] != molecular[j][0]:
                 key = f"{molecular[i][0]}-{molecular[j][0]}"
             else:
@@ -108,17 +115,9 @@ class DataBase:
                 else:
                     key = f"{molecular[i][0]}-{molecular[j][0]}-O"
 
-            input_mat = np.load(
-                self.dir_input / f"input_dft_{name}_{i}_{j}.npy"
-            ).flatten()
-            middle_mat = np.load(
-                self.dir_input / f"input_cc_{name}_{i}_{j}.npy"
-            ).flatten()
-            output_mat = delta_exc_cc[i, j] * 1000
-
-            self.input[key][f"{name}_{i}_{j}"] = input_mat
-            self.middle[key][f"{name}_{i}_{j}"] = middle_mat - input_mat
-            self.output[key][f"{name}_{i}_{j}"] = output_mat[np.newaxis]
+            self.input[key][f"{name}_{i}_{j}"] = input_mat[slice_].flatten()
+            self.middle[key][f"{name}_{i}_{j}"] = middle_mat[slice_].flatten()
+            self.output[key][f"{name}_{i}_{j}"] = output_mat[slice_].flatten() * 1000
 
     def check(self, model_list=None, if_equilibrium=True):
         """
@@ -218,9 +217,7 @@ class DataBase:
 
             dm1_middle_real[
                 dft2cc.atom_info["slice"][i], dft2cc.atom_info["slice"][j]
-            ] = (middle_real + input_mat).reshape(
-                NAO[molecular[i][0]], NAO[molecular[j][0]]
-            )
+            ] = middle_real.reshape(NAO[molecular[i][0]], NAO[molecular[j][0]])
             exc_real += output_real.copy()[0]
 
             if not (model_list is None):
@@ -241,7 +238,7 @@ class DataBase:
                 output_mat = output_mat.detach().cpu().numpy()
             else:
                 middle_mat = middle_real.copy()
-                middle_mat += input_mat
+                # middle_mat += input_mat
                 output_mat = output_real.copy()
 
             dm1_middle[dft2cc.atom_info["slice"][i], dft2cc.atom_info["slice"][j]] = (
@@ -259,20 +256,6 @@ class DataBase:
 
         rho = dft.numint.eval_rho(dft2cc.mol, ao_value, dm1_middle, xctype="GGA")
         exc_cc_grids = dft.libxc.eval_xc("b3lyp", rho)[0]
-
-        eri = dft2cc.mol.intor("int2e")
-        h1e = dft2cc.mol.intor("int1e_nuc") + dft2cc.mol.intor("int1e_kin")
-        ek_mat_cc = np.einsum("pqrs,pr,qs->qs", eri, dm1_middle, dm1_middle)
-        exc_cc = (
-            np.einsum("i,i,i->", exc_cc_grids, rho[0], weights)
-            - np.sum(ek_mat_cc) * 0.05
-        )
-        e_dft = (
-            exc_cc
-            + np.einsum("pqrs,pq,rs", eri, dm1_middle, dm1_middle) / 2
-            + np.sum(h1e * dm1_middle)
-            + dft2cc.mol.energy_nuc()
-        )
 
         h1e = dft2cc.mol.intor("int1e_nuc") + dft2cc.mol.intor("int1e_kin")
         eri = dft2cc.mol.intor("int2e")
@@ -312,9 +295,9 @@ class DataBase:
             ene_loss_i = exc + 1000 * (e_dft - self.data[name]["e_cc"])
             ene_loss_i_1 = exc - exc_real
             ene_loss_i_2 = 1000 * (e_dft - e_dft_real)
-            if ene_loss_i > 1e-3:
-                print("")
-                print(f"name: {name}, ene_loss_i: {ene_loss_i:7.4f}")
+            # if ene_loss_i > 1e-3:
+            #     print("")
+            #     print(f"name: {name}, ene_loss_i: {ene_loss_i:7.4f}")
 
             rho_loss_i = 0
         else:
