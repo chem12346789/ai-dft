87dd5f3a923a09cde288e86aea20b125bc7ffb97
auto <chenzihao@bright91.cm.cluster>
2024-05-22 12:45:46 +0800

    240522

--- gen_data_script/gen_data_template.bash
+++ gen_data_script/gen_data_template.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #slurm options
-#SBATCH -p cpu
-#SBATCH -c 14
-#SBATCH -J MOL_BASIS_START_END_STEP
+#SBATCH -p gpu
+#SBATCH -J train-ccdft-CHECKPOINT-HIDDEN_SIZE
 #SBATCH -o log/%j.log
+#SBATCH --gres=gpu:1
+#SBATCH --exclude=gpu[004-006]
 
 ## user's own commands below
 export OMP_NUM_THREADS=14
@@ -15,4 +16,4 @@ export PYSCF_TMPDIR=~/workdir/tmp
 export PYTHONPATH=~/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
-~/anaconda3/bin/python gen_dm_ene.py -dl START END STEP -b BASIS --extend_atom 2 3 4 --extend_xyz 0 1 2 --name_mol MOL
+~/anaconda3/bin/python gen_dm_ene.py -dl START END STEP -b BASIS --extend_atom 0 --extend_xyz 0 1 2 --name_mol MOL
