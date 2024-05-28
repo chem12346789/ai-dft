667b3f9c32d252b74b880ac3b245484fe69e5acd
auto <chenzihao@bright91.cm.cluster>
2024-05-28 20:26:38 +0800

    240528

--- train_script/train-template.bash
+++ train_script/train-template.bash
@@ -15,5 +15,5 @@ export OPENBLAS_NUM_THREADS=12
 export PYTHONPATH=~/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
-~/anaconda3/bin/python train.py -dl -0.5 0.5 11 -b cc-pvdz --extend_atom 0 1 2 --extend_xyz 0 1 2 --eval_step 100 --load CHECKPOINT --batch_size 8196 --epoch 100000 --hidden_size HIDDEN_SIZE --residual 2
+~/anaconda3/bin/python train.py -dl -0.5 0.5 11 -b cc-pvdz --extend_atom 0 1 24 --extend_xyz 0 1 2 --eval_step 100 --load CHECKPOINT --batch_size 8196 --epoch 100000 --hidden_size HIDDEN_SIZE --residual 2
 # ~/anaconda3/bin/python train.py -dl -0.5 0.5 51 -b cc-pvdz --extend_atom 0 1 2 3 4 --extend_xyz 0 1 2 --eval_step 100 --load CHECKPOINT --batch_size 262144 --epoch 100000 --hidden_size HIDDEN_SIZE
