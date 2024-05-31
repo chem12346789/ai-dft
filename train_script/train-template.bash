d61b971d3b5a8d0e1c5df43bdfa5d582238c3d58
auto <chenzihao@bright91.cm.cluster>
2024-05-31 15:14:18 +0800

    240531

--- train_script/train-template.bash
+++ train_script/train-template.bash
@@ -4,7 +4,6 @@
 #SBATCH -p gpu
 #SBATCH -J train-ccdft-CHECKPOINT-HIDDEN_SIZE
 #SBATCH -o log/%j.log
-#SBATCH --gres=gpu:1
 #SBATCH --exclude=gpu[004-006]
 
 ## user's own commands below
@@ -15,4 +14,4 @@ export OPENBLAS_NUM_THREADS=12
 export PYTHONPATH=~/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
-~/anaconda3/bin/python train.py -dl -0.5 0.5 51 -b cc-pvdz --extend_atom 0 1 2 3 4 --extend_xyz 0 1 2 --eval_step 100 --load CHECKPOINT --batch_size 262144 --epoch 100000 --hidden_size HIDDEN_SIZE
+~/anaconda3/bin/python train.py -dl -0.5 0.5 11 -b cc-pvdz --extend_atom 0 1 2 --extend_xyz 0 1 2 --eval_step 100 --load CHECKPOINT --batch_size 4096 --epoch 100000 --hidden_size HIDDEN_SIZE --residual 1 --num_layer 3
