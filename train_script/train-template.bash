c076a186ce815ea33ba6da5362a2d4eca6445762
auto <chenzihao@bright91.cm.cluster>
2024-06-05 12:47:06 +0800

    240605

--- train_script/train-template.bash
+++ train_script/train-template.bash
@@ -4,7 +4,7 @@
 #SBATCH -p gpu
 #SBATCH -J train-ccdft-CHECKPOINT-HIDDEN_SIZE
 #SBATCH -o log/%j.log
-#SBATCH --exclude=gpu[001,006]
+#SBATCH --exclude=gpu[006,007]
 #SBATCH --mem-per-gpu=20G
 
 ## user's own commands below
@@ -18,4 +18,4 @@ CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH=~/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
-~/anaconda3/bin/python train.py -dl -0.5 0.5 11 -b cc-pvdz --extend_atom 0 1 2 --extend_xyz 0 1 2 --eval_step 100 --load CHECKPOINT --batch_size 4096 --epoch 100000 --hidden_size HIDDEN_SIZE --num_layer 7 --residual 1 ENE_GRID_FACTOR
+~/anaconda3/bin/python train.py -dl -0.5 0.5 11 -b cc-pvdz --extend_atom 0 1 2 --extend_xyz 0 1 2 --eval_step 100 --load CHECKPOINT --batch_size 4096 --epoch 100000 --hidden_size HIDDEN_SIZE --num_layer 9 --residual 1 ENE_GRID_FACTOR
