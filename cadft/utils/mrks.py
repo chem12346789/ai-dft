f2102dbefcef837a40886e35f2b13398bc1a17e7
czh <pb142060@mail.ustc.edu.cn>
2024-06-13 17:29:48 +0800

    240613

--- cadft/utils/mrks.py
+++ cadft/utils/mrks.py
@@ -13,13 +13,12 @@ from cadft.utils.Grids import Grid
 AU2KJMOL = 2625.5
 
 
-def mrks(
-    self,
-    frac_old,
-):
+def mrks(self, frac_old, load_inv=True):
     """
     Generate 1-RDM.
     """
+    Path(f"data/grids/saved_data/{self.name}").mkdir(parents=True, exist_ok=True)
+
     mdft = pyscf.scf.RKS(self.mol)
     mdft.xc = "b3lyp"
     mdft.kernel()
@@ -88,7 +87,13 @@ def mrks(
     rho_cc_half = pyscf.dft.numint.eval_rho(self.mol, ao_0, dm1_cc / 2) + 1e-14
     exc_grids = np.zeros_like(rho_cc[0])
 
-    if False:
+    if load_inv and Path(f"data/grids/saved_data/{self.name}/exc_grids.npy").exists():
+        exc_grids = np.load(f"data/grids/saved_data/{self.name}/exc_grids.npy")
+        exc_over_rho_grids = np.load(
+            f"data/grids/saved_data/{self.name}/exc_over_rho_grids.npy"
+        )
+    else:
+        print("Calculating exc_grids")
         expr_rinv_dm2_r = oe.contract_expression(
             "ijkl,i,j,kl->",
             0.5 * (dm2_cc - oe.contract("pq,rs->pqrs", dm1_cc, dm1_cc)),
@@ -114,156 +119,174 @@ def mrks(
 
         print(f"Error: {(1e3 * error):.5f} mHa")
         np.save(
-            f"data/grids/saved_data/exc_over_rho_grids_{self.name}.npy",
-            exc_over_rho_grids,
-        )
-        np.save(
-            f"data/grids/saved_data/exc_grids_{self.name}.npy",
+            f"data/grids/saved_data/{self.name}/exc_grids.npy",
             exc_grids,
         )
+        np.save(
+            f"data/grids/saved_data/{self.name}/exc_over_rho_grids.npy",
+            exc_over_rho_grids,
+        )
+
+    if load_inv and Path(f"data/grids/saved_data/{self.name}/emax.npy").exists():
+        emax = np.load(f"data/grids/saved_data/{self.name}/emax.npy")
+        taup_rho_wf = np.load(f"data/grids/saved_data/{self.name}/taup_rho_wf.npy")
+        tau_rho_wf = np.load(f"data/grids/saved_data/{self.name}/tau_rho_wf.npy")
+        v_vxc_e_taup = np.load(f"data/grids/saved_data/{self.name}/v_vxc_e_taup.npy")
     else:
-        exc_over_rho_grids = np.load(
-            f"data/grids/saved_data/exc_over_rho_grids_{self.name}.npy"
+        generalized_fock = dm1_cc_mo @ h1_mo + oe.contract(
+            "rsnq,rsmq->mn", eri_mo, dm2_cc_mo
         )
-        exc_grids = np.load(f"data/grids/saved_data/exc_grids_{self.name}.npy")
+        generalized_fock = 0.5 * (generalized_fock + generalized_fock.T)
 
-    generalized_fock = dm1_cc_mo @ h1_mo + oe.contract(
-        "rsnq,rsmq->mn", eri_mo, dm2_cc_mo
-    )
-    generalized_fock = 0.5 * (generalized_fock + generalized_fock.T)
-
-    eig_e, eig_v = np.linalg.eigh(generalized_fock)
-    eig_v = mo @ eig_v
-    eig_e = eig_e / 2
-    e_bar_r_wf = (
-        oe.contract(
-            "i,mi,ni,pm,pn->p",
-            eig_e,
-            eig_v,
-            eig_v,
-            ao_0,
-            ao_0,
-        )
-        / rho_cc_half
-    )
-
-    emax = np.max(e_bar_r_wf)
-    v_vxc_e_taup = -e_bar_r_wf
-
-    eigs_e_dm1, eigs_v_dm1 = np.linalg.eigh(dm1_cc_mo)
-    eigs_v_dm1 = mo @ eigs_v_dm1
-    eigs_e_dm1 = eigs_e_dm1 / 2
-
-    taup_rho_wf = gen_taup_rho(
-        rho_cc_half,
-        eigs_v_dm1,
-        eigs_e_dm1,
-        oe_taup_rho,
-        backend="torch",
-    )
-
-    tau_rho_wf = gen_tau_rho(
-        rho_cc_half,
-        eigs_v_dm1,
-        eigs_e_dm1,
-        oe_tau_rho,
-        backend="torch",
-    )
-
-    eigvecs_inv = mf.mo_energy.copy()
-    mo_inv = mo.copy()
-    dm1_inv = dm1_cc / 2
-    vj_inv = mf.get_jk(self.mol, dm1_inv * 2, 1)[0]
-    v_vxc_e_taup += exc_over_rho_grids * 2 + taup_rho_wf / rho_cc_half
-
-    vxc_inv = pyscf.dft.libxc.eval_xc(
-        "B88,P86",
-        pyscf.dft.numint.eval_rho(
-            self.mol,
-            ao_value[:4, :, :],
-            dm1_inv * 2,
-            xctype="GGA",
-        ),
-    )[1][0]
-
-    oe_ebar_r_ks = oe.contract_expression(
-        "i,mi,ni,pm,pn->p",
-        (nocc,),
-        (norb, nocc),
-        (norb, nocc),
-        ao_0,
-        ao_0,
-        constants=[3, 4],
-        optimize="optimal",
-    )
-
-    oe_fock = oe.contract_expression(
-        "p,p,pa,pb->ab",
-        np.shape(ao_0[:, 0]),
-        np.shape(ao_0[:, 0]),
-        ao_0,
-        ao_0,
-        constants=[2, 3],
-        optimize="optimal",
-    )
-
-    for i in range(25000):
-        vj_inv = hybrid(mf.get_jk(self.mol, 2 * dm1_inv, 1)[0], vj_inv)
-        dm1_inv_r = pyscf.dft.numint.eval_rho(self.mol, ao_0, dm1_inv) + 1e-14
-
-        potential_shift = emax - np.max(eigvecs_inv[:nocc])
-        ebar_ks = (
-            oe_ebar_r_ks(
-                eigvecs_inv[:nocc] + potential_shift,
-                mo_inv[:, :nocc],
-                mo_inv[:, :nocc],
+        eig_e, eig_v = np.linalg.eigh(generalized_fock)
+        eig_v = mo @ eig_v
+        eig_e = eig_e / 2
+        e_bar_r_wf = (
+            oe.contract(
+                "i,mi,ni,pm,pn->p",
+                eig_e,
+                eig_v,
+                eig_v,
+                ao_0,
+                ao_0,
             )
-            / dm1_inv_r
+            / rho_cc_half
         )
 
-        taup_rho_ks = gen_taup_rho(
-            dm1_inv_r,
-            mo_inv[:, :nocc],
-            np.ones(nocc),
+        emax = np.max(e_bar_r_wf)
+        v_vxc_e_taup = -e_bar_r_wf
+
+        eigs_e_dm1, eigs_v_dm1 = np.linalg.eigh(dm1_cc_mo)
+        eigs_v_dm1 = mo @ eigs_v_dm1
+        eigs_e_dm1 = eigs_e_dm1 / 2
+
+        taup_rho_wf = gen_taup_rho(
+            rho_cc_half,
+            eigs_v_dm1,
+            eigs_e_dm1,
             oe_taup_rho,
             backend="torch",
         )
 
-        vxc_inv_old = vxc_inv.copy()
-        vxc_inv = v_vxc_e_taup + ebar_ks - taup_rho_ks / dm1_inv_r
-        error_vxc = np.linalg.norm((vxc_inv - vxc_inv_old) * weights)
-        vxc_inv = hybrid(vxc_inv, vxc_inv_old)
+        tau_rho_wf = gen_tau_rho(
+            rho_cc_half,
+            eigs_v_dm1,
+            eigs_e_dm1,
+            oe_tau_rho,
+            backend="torch",
+        )
 
-        xc_v = oe_fock(vxc_inv, weights, backend="torch")
-        eigvecs_inv, mo_inv = np.linalg.eigh(mat_hs @ (h1e + vj_inv + xc_v) @ mat_hs)
-        mo_inv = mat_hs @ mo_inv
-        dm1_inv_old = dm1_inv.copy()
-        dm1_inv = mo_inv[:, :nocc] @ mo_inv[:, :nocc].T
-        error_dm1 = np.linalg.norm(dm1_inv - dm1_inv_old)
+        v_vxc_e_taup += exc_over_rho_grids * 2 + taup_rho_wf / rho_cc_half
 
-        if i % 100 == 0:
-            print(
-                f"step:{i:<8}",
-                f"error of vxc: {error_vxc::<10.5e}",
-                f"dm: {error_dm1::<10.5e}",
-                f"shift: {potential_shift::<10.5e}",
+        np.save(f"data/grids/saved_data/{self.name}/emax.npy", emax)
+        np.save(f"data/grids/saved_data/{self.name}/taup_rho_wf.npy", taup_rho_wf)
+        np.save(f"data/grids/saved_data/{self.name}/tau_rho_wf.npy", tau_rho_wf)
+        np.save(f"data/grids/saved_data/{self.name}/v_vxc_e_taup.npy", v_vxc_e_taup)
+
+    if load_inv and Path(f"data/grids/saved_data/{self.name}/dm1_inv.npy").exists():
+        dm1_inv = np.load(f"data/grids/saved_data/{self.name}/dm1_inv.npy")
+        vxc_inv = np.load(f"data/grids/saved_data/{self.name}/vxc_inv.npy")
+        tau_rho_ks = np.load(f"data/grids/saved_data/{self.name}/tau_rho_ks.npy")
+        taup_rho_ks = np.load(f"data/grids/saved_data/{self.name}/taup_rho_ks.npy")
+    else:
+        eigvecs_inv = mf.mo_energy.copy()
+        mo_inv = mo.copy()
+        dm1_inv = dm1_cc / 2
+        vj_inv = mf.get_jk(self.mol, dm1_inv * 2, 1)[0]
+        vxc_inv = pyscf.dft.libxc.eval_xc(
+            "B88,P86",
+            pyscf.dft.numint.eval_rho(
+                self.mol,
+                ao_value[:4, :, :],
+                dm1_inv * 2,
+                xctype="GGA",
+            ),
+        )[1][0]
+
+        oe_ebar_r_ks = oe.contract_expression(
+            "i,mi,ni,pm,pn->p",
+            (nocc,),
+            (norb, nocc),
+            (norb, nocc),
+            ao_0,
+            ao_0,
+            constants=[3, 4],
+            optimize="optimal",
+        )
+
+        oe_fock = oe.contract_expression(
+            "p,p,pa,pb->ab",
+            np.shape(ao_0[:, 0]),
+            np.shape(ao_0[:, 0]),
+            ao_0,
+            ao_0,
+            constants=[2, 3],
+            optimize="optimal",
+        )
+
+        for i in range(25000):
+            vj_inv = hybrid(mf.get_jk(self.mol, 2 * dm1_inv, 1)[0], vj_inv)
+            dm1_inv_r = pyscf.dft.numint.eval_rho(self.mol, ao_0, dm1_inv) + 1e-14
+
+            potential_shift = emax - np.max(eigvecs_inv[:nocc])
+            ebar_ks = (
+                oe_ebar_r_ks(
+                    eigvecs_inv[:nocc] + potential_shift,
+                    mo_inv[:, :nocc],
+                    mo_inv[:, :nocc],
+                )
+                / dm1_inv_r
             )
-        if (i > 0) and (error_vxc < 1e-6):
-            print(
-                f"step:{i:<8}",
-                f"error of vxc: {error_vxc::<10.5e}",
-                f"dm: {error_dm1::<10.5e}",
-                f"shift: {potential_shift::<10.5e}",
-            )
-            break
 
-    tau_rho_ks = gen_tau_rho(
-        dm1_inv_r,
-        mo_inv[:, :nocc],
-        np.ones(nocc),
-        oe_tau_rho,
-        backend="torch",
-    )
+            taup_rho_ks = gen_taup_rho(
+                dm1_inv_r,
+                mo_inv[:, :nocc],
+                np.ones(nocc),
+                oe_taup_rho,
+                backend="torch",
+            )
+
+            vxc_inv_old = vxc_inv.copy()
+            vxc_inv = v_vxc_e_taup + ebar_ks - taup_rho_ks / dm1_inv_r
+            error_vxc = np.linalg.norm((vxc_inv - vxc_inv_old) * weights)
+            vxc_inv = hybrid(vxc_inv, vxc_inv_old)
+
+            xc_v = oe_fock(vxc_inv, weights, backend="torch")
+            eigvecs_inv, mo_inv = np.linalg.eigh(
+                mat_hs @ (h1e + vj_inv + xc_v) @ mat_hs
+            )
+            mo_inv = mat_hs @ mo_inv
+            dm1_inv_old = dm1_inv.copy()
+            dm1_inv = mo_inv[:, :nocc] @ mo_inv[:, :nocc].T
+            error_dm1 = np.linalg.norm(dm1_inv - dm1_inv_old)
+
+            if i % 100 == 0:
+                print(
+                    f"step:{i:<8}",
+                    f"error of vxc: {error_vxc::<10.5e}",
+                    f"dm: {error_dm1::<10.5e}",
+                    f"shift: {potential_shift::<10.5e}",
+                )
+            if (i > 0) and (error_vxc < 1e-6):
+                print(
+                    f"step:{i:<8}",
+                    f"error of vxc: {error_vxc::<10.5e}",
+                    f"dm: {error_dm1::<10.5e}",
+                    f"shift: {potential_shift::<10.5e}",
+                )
+                break
+
+        tau_rho_ks = gen_tau_rho(
+            dm1_inv_r,
+            mo_inv[:, :nocc],
+            np.ones(nocc),
+            oe_tau_rho,
+            backend="torch",
+        )
+        np.save(f"data/grids/saved_data/{self.name}/dm1_inv.npy", dm1_inv)
+        np.save(f"data/grids/saved_data/{self.name}/vxc_inv.npy", vxc_inv)
+        np.save(f"data/grids/saved_data/{self.name}/tau_rho_ks.npy", tau_rho_ks)
+        np.save(f"data/grids/saved_data/{self.name}/taup_rho_ks.npy", taup_rho_ks)
 
     kin_correct = 2 * np.sum((tau_rho_wf - tau_rho_ks) * weights)
     kin_correct1 = 2 * np.sum((taup_rho_wf - taup_rho_ks) * weights)
@@ -288,9 +311,10 @@ def mrks(
 
             for i_atom in range(self.mol.natm):
                 distance = np.linalg.norm(self.mol.atom_coords()[i_atom] - coord)
-                cut_off = 1e-2
+                cut_off = 1e-4
                 if distance < cut_off:
-                    distance = 2 / cut_off - 1 / cut_off / cut_off * distance
+                    # distance = 2 / cut_off - 1 / cut_off / cut_off * distance
+                    distance = cut_off
                 exc_grids[i] -= (
                     (rho_cc[0][i] - inv_r[i])
                     * self.mol.atom_charges()[i_atom]
