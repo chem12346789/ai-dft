de158b2557a5ecdb093b41c0e55844fc61a35b6b
auto <chenzihao@bright91.cm.cluster>
2024-07-06 17:13:40 +0800

    240706

--- gen_data_script/mrks1.sh
+++ gen_data_script/mrks1.sh
@@ -2,13 +2,14 @@
 
 #slurm options
 #SBATCH -p gpu
+#SBATCH --gres=gpu:1
 #SBATCH -J cc-pCVDZ1
-#SBATCH -o log/cc-pCVDZ1.out
+#SBATCH -o log/cc-pCVDZ1.log
 #SBATCH --exclude=gpu[001,003-007]
 
-export OMP_NUM_THREADS=12
-export MKL_NUM_THREADS=12
-export OPENBLAS_NUM_THREADS=12
+export OMP_NUM_THREADS=20
+export MKL_NUM_THREADS=20
+export OPENBLAS_NUM_THREADS=20
 
 export PYSCF_MAX_MEMORY=40000
 export PYSCF_TMPDIR=~/workdir/tmp
@@ -16,4 +17,4 @@ export HDF5_USE_FILE_LOCKING=FALSE
 export PYTHONPATH=~/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
-nohup sh -c '~/anaconda3/bin/python gen_dm_ene.py -dl -0.05 -0.05 1 -b cc-pCVDZ --extend_atom 0 --extend_xyz 0 --name_mol pentane cyclopentane isopentane benzene --load_inv True >log/cc-pCVDZ1.out' >log/cc-pCVDZ1.sbatch &
+~/anaconda3/bin/python gen_dm_ene.py -dl -0.5 0.5 11 -b cc-pCVDZ --extend_atom 0 --extend_xyz 0 --name_mol methane ethylene propane propylene cyclopropene butane isobutane
--- test_script/test0.bash
+++ test_script/test0.bash
@@ -3,20 +3,19 @@
 #slurm options
 #SBATCH -p gpu
 #SBATCH -J test-ccdft-0
-#SBATCH -o log/%j.log
-#SBATCH --exclude=gpu[006]
+#SBATCH -o log/test0.log
+#SBATCH --exclude=gpu[001,002,004-007]
 
 ## set environment variables
-export OMP_NUM_THREADS=12
-export MKL_NUM_THREADS=12
-export OPENBLAS_NUM_THREADS=12
+export OMP_NUM_THREADS=20
+export MKL_NUM_THREADS=20
+export OPENBLAS_NUM_THREADS=20
 
-export NVIDIA_VISIBLE_DEVICES=1
-export CUDA_VISIBLE_DEVICES=$(nvidia-smi --query-gpu=memory.free,index --format=csv,nounits,noheader | sort -nr | head -1 | awk '{ print $NF }')
-
+export PYSCF_MAX_MEMORY=40000
 export PYSCF_TMPDIR=~/workdir/tmp
+export HDF5_USE_FILE_LOCKING=FALSE
 export PYTHONPATH=~/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/anaconda3/lib:$LD_LIBRARY_PATH
 
 ## user's own commands below
-~/anaconda3/bin/python test.py -dl -0.05 -0.05 1 -b cc-pCVDZ --extend_atom 0 --extend_xyz 0 --load 2024-07-03-14-43-44 --name_mol methane ethane ethylene acetylene propane pentane cyclopentane isopentane benzene --hidden_size 32 --residual 0 --num_layer 4 --precision float32 >log/test0.sbatch
+~/anaconda3/bin/python test.py -dl -0.45 0.45 10 -b cc-pCVDZ --extend_atom 0 --extend_xyz 0 --load 2024-07-05-16-20-29 --name_mol methane ethane ethylene acetylene propane pentane cyclopentane isopentane benzene --hidden_size 64 --residual 0 --num_layer 4 --precision float32 >log/test0.out
