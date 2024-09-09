a6b430a74e594addfb8b9046e02e5657de75f5a5
dhem <>
2024-09-09 15:34:09 +0800

    240909

--- cadft/test_rks_pyscf.py
+++ cadft/test_rks_pyscf.py
@@ -92,8 +92,9 @@ def test_rks_pyscf(
                 scf_rho_r3 = pyscf.dft.numint.eval_rho(
                     dft2cc.mol, dft2cc.ao_1, dm, xctype="GGA"
                 )
-                vxc_scf = modeldict.get_v(scf_rho_r3, dft2cc.grids) + vexc_lda[1][0]
-                exc_scf = modeldict.get_e(scf_rho_r3, dft2cc.grids) + vexc_lda[0]
+                vexc_b3lyp = pyscf.dft.libxc.eval_xc("b3lyp", scf_rho_r3)
+                vxc_scf = modeldict.get_v(scf_rho_r3, dft2cc.grids) + vexc_b3lyp[0]
+                exc_scf = modeldict.get_e(scf_rho_r3, dft2cc.grids) + vexc_b3lyp[0]
 
         vxc_mat = oe_fock(vxc_scf, dft2cc.grids.weights)
         exc_ene = np.sum(exc_scf * scf_rho_r * dft2cc.grids.weights)
@@ -151,7 +152,8 @@ def test_rks_pyscf(
                 scf_rho_r3 = pyscf.dft.numint.eval_rho(
                     dft2cc.mol, dft2cc.ao_1, dms, xctype="GGA"
                 )
-                vxc_scf = modeldict.get_v(scf_rho_r3, dft2cc.grids) + vexc_lda[1][0]
+                vexc_b3lyp = pyscf.dft.libxc.eval_xc("b3lyp", scf_rho_r3)
+                vxc_scf = modeldict.get_v(scf_rho_r3, dft2cc.grids) + vexc_b3lyp[0]
 
         wv = dft2cc.grids.weights * vxc_scf
         aow = np.einsum("gi,g->gi", dft2cc.ao_1[0], wv)
