ecc69246db276ccb30f53f3bbd998c2326e30206
czh <pb142060@mail.ustc.edu.cn>
2024-11-26 19:51:33 +0800

    241126

--- gen_data_script/gen_data_template_direct.bash
+++ gen_data_script/gen_data_template_direct.bash
@@ -14,4 +14,4 @@ export NVIDIA_VISIBLE_DEVICES=1
 # export CUDA_VISIBLE_DEVICES=NUMBER_OF_GPU
 export CUDA_VISIBLE_DEVICES=$(nvidia-smi --query-gpu=power.draw,index --format=csv,nounits,noheader | sort -n | head -1 | awk '{ print $NF }')
 
-~/anaconda3/envs/pyscf3.8/bin/python gen_dm_ene.py -dl START END STEP -b BASIS --extend_atom EXTEND_ATOM --extend_xyz 0 --name_mol MOL --load_inv True
+~/anaconda3/envs/pyscf/bin/python gen_dm_ene.py -dl START END STEP -b BASIS --extend_atom EXTEND_ATOM --extend_xyz 0 --name_mol MOL --load_inv True
