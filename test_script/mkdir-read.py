4e6dfb1404c4a6ab7b92fbd3014a823e29296262
dhem <>
2024-12-07 22:03:42 +0800

    241207

--- test_script/mkdir-read.py
+++ test_script/mkdir-read.py
@@ -44,7 +44,8 @@ work_bash = work_dir / "validate-template.bash"
 for (checkpoint_hidden_size,) in itertools.product(
     [
         # "checkpoint-ccdft_2024-11-23-17-12-12_1_64_1_4_-1",
-        "checkpoint-ccdft_2024-12-05-00-15-54_1_64_1_5_0.-1",
+        # "checkpoint-ccdft_2024-12-05-00-15-54_1_64_1_5_0.-1",
+        "checkpoint-ccdft_2024-12-06-13-36-03_1_64_1_5_1.-1",
     ],
 ):
     (
